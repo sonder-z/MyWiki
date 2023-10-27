8694dc71a5b1d2b48da4efb78edb321b0d04b3e7
sonder-z <1721886276@qq.com>
2023-10-27 20:35:02 +0800

    测试

---
+++ src/main/java/com/example/wiki/controller/testContoller.java
@@ -0,0 +1,10 @@
+package com.example.wiki.controller;
+
+public class testContoller {
+    private String name;
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
