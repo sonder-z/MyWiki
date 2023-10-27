55b4538fe3bdcacbc3a371a09fed0a19b86dd86b
sonder-z <1721886276@qq.com>
2023-10-27 22:24:17 +0800

    2-5 修改启动文案

--- src/main/java/com/example/wiki/WikiApplication.java
+++ src/main/java/com/example/wiki/WikiApplication.java
@@ -1,14 +1,22 @@
 package com.example.wiki;
 
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class WikiApplication {
+    private static final Logger LOG = LoggerFactory.getLogger(WikiApplication.class);
 
 //    添加一行注释试试
     public static void main(String[] args) {
-        SpringApplication.run(WikiApplication.class, args);
+        SpringApplication app = new SpringApplication(WikiApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        LOG.info("启动成功！！");
+        LOG.info("地址: \thttp://127.0.0.1:{}", env.getProperty("server.port"));
+
     }
 
 }
