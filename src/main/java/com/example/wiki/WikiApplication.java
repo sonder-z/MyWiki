dbbe7d4254a924ee500a12d3d9c9766ea0a1b980
sonder-z <1721886276@qq.com>
2023-10-27 20:31:40 +0800

    2-2 init

--- src/main/java/com/example/wiki/WikiApplication.java
+++ src/main/java/com/example/wiki/WikiApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WikiApplication {
 
+//    添加一行注释试试
     public static void main(String[] args) {
         SpringApplication.run(WikiApplication.class, args);
     }
