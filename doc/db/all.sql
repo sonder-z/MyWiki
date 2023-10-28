d15afea41f07d8b5fb1dd6d25d1cb2503fcf02d4
sonder-z <1721886276@qq.com>
2023-10-28 16:28:59 +0800

    3-5 集成mybatis-generator,生成demo表代码

--- doc/db/all.sql
+++ doc/db/all.sql
@@ -7,3 +7,12 @@ create table `test`(
 
 insert into `test` (`id`, `name`) values (1,'测试1');
 insert into `test` (`id`, `name`) values (2,'测试2');
+
+drop table if exists `demo`;
+create table `demo` (
+                        `id` bigint not null comment 'id',
+                        `name` varchar(50) comment '名称',
+                        primary key (`id`)
+) engine=innodb default charset=utf8mb4 comment='测试';
+
+insert into `demo` (id, name) values (1, '测试');
