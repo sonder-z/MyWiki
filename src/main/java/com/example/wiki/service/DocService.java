47f1a4666b69412a1829951ead169b03883039dd
sonder-z <1721886276@qq.com>
2023-10-31 11:33:13 +0800

    10-3 同一个IP一天只能对一个文档点赞一次

--- src/main/java/com/example/wiki/service/DocService.java
+++ src/main/java/com/example/wiki/service/DocService.java
@@ -3,6 +3,8 @@ package com.example.wiki.service;
 import com.example.wiki.domain.Content;
 import com.example.wiki.domain.Doc;
 import com.example.wiki.domain.DocExample;
+import com.example.wiki.exception.BusinessException;
+import com.example.wiki.exception.BusinessExceptionCode;
 import com.example.wiki.mapper.ContentMapper;
 import com.example.wiki.mapper.DocMapper;
 import com.example.wiki.mapper.DocMapperCust;
@@ -11,6 +13,8 @@ import com.example.wiki.req.DocSaveReq;
 import com.example.wiki.resp.DocQueryResp;
 import com.example.wiki.resp.PageResp;
 import com.example.wiki.util.CopyUtil;
+import com.example.wiki.util.RedisUtil;
+import com.example.wiki.util.RequestContext;
 import com.example.wiki.util.SnowFlake;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
@@ -35,6 +39,9 @@ public class DocService {
 
     @Resource
     private SnowFlake snowFlake;
+
+    @Resource
+    private RedisUtil redisUtil;
 
     private static final Logger LOG = LoggerFactory.getLogger(DocService.class);
 
@@ -124,7 +131,13 @@ public class DocService {
     }
 
     public void vote (Long id) {
-        docMapperCust.IncreaseVoteCount(id);
+//        docMapperCust.IncreaseVoteCount(id);
+        String ip = RequestContext.getRemoteAddr();
+        if (redisUtil.validateRepeat("DOC_VOTE_" + id + "_" + ip, 3600 * 24)){
+            docMapperCust.IncreaseVoteCount(id);
+        } else {
+            throw new BusinessException(BusinessExceptionCode.VOTE_REPEAT);
+        }
     }
 
 }
