59fafc99a3f7001f38a5e33b0c31787cd7071527
sonder-z <1721886276@qq.com>
2023-11-01 10:02:07 +0800

    10-10 加入事务注解

--- src/main/java/com/example/wiki/service/DocService.java
+++ src/main/java/com/example/wiki/service/DocService.java
@@ -22,6 +22,7 @@ import com.github.pagehelper.PageInfo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.ObjectUtils;
 
 import javax.annotation.Resource;
@@ -83,6 +84,7 @@ public class DocService {
     /*
         插入或更新
      */
+    @Transactional
     public void save(DocSaveReq req) {
         //copy一份请求实体作为实体类
         Doc doc = CopyUtil.copy(req, Doc.class);
